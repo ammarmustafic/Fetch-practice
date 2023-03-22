84dd04dfeda42cacb10a7dbeb0bd40be4f8c381c
Ammar <mustaficammar0@gmail.com>
2023-03-22 13:20:44 +0100

    add new files

---
+++ chuck norris's jokes/app.js
@@ -0,0 +1,30 @@
+async function getAllJokes() {
+    let catjokes = await fetch("https://api.chucknorris.io/jokes/categories");
+    let response = catjokes.json();
+    return response;
+}
+
+let cont = document.querySelector(".container");
+let jokes;
+
+getAllJokes().then((el) => {
+    jokes = el;
+    jokes.forEach(element => {
+        createCategory(element);
+    });
+    console.log(jokes)
+})
+
+
+function createCategory(element) {
+    let a = document.createElement("div");
+    a.className = "a";
+
+    let category = document.createElement("p");
+    category.innerText = element;
+    a.appendChild(category);
+    a.addEventListener("click", () => {
+        window.location.href = `index1.html?id=${element}`;
+      });
+    cont.appendChild(a);
+}
---
+++ chuck norris's jokes/app1.js
@@ -0,0 +1,33 @@
+let params = new URLSearchParams(window.location.search);
+let categoryId = params.get("id");
+
+async function getRandomJoke(category) {
+  let jokeResponse = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
+  let jokeData = await jokeResponse.json();
+  return jokeData;
+}
+
+let cont = document.querySelector(".container");
+getRandomJoke(categoryId).then((joke) => {
+  displayJoke(joke);
+});
+
+function displayJoke(joke) {
+  let jokeDiv = document.createElement("div");
+  jokeDiv.className = "joke";
+
+  let jokeText = document.createElement("p");
+  jokeText.innerText = joke.value;
+  jokeDiv.appendChild(jokeText);
+
+  let newJokeButton = document.createElement("button");
+  newJokeButton.innerText = "Get Another Joke";
+  newJokeButton.addEventListener("click", () => {
+    getRandomJoke(categoryId).then((newJoke) => {
+      jokeText.innerText = newJoke.value;
+    });
+  });
+  jokeDiv.appendChild(newJokeButton);
+
+  cont.appendChild(jokeDiv);
+}
